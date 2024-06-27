ee0fca0893a9e8f42d86331ff45dd75bb30c9ea0
Hansani Gunathilaka <hansinima20003@gmail.com>
2024-06-27 19:33:56 +0530

    finish signup backend

---
+++ src/main/java/org/chandima/registration/Service/UserService.java
@@ -0,0 +1,23 @@
+package org.chandima.registration.Service;
+
+import org.chandima.registration.DTO.UserDTO;
+import org.chandima.registration.Entity.User;
+import org.chandima.registration.Repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public UserDTO saveUser(UserDTO userDTO){
+        userRepo.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
+}
